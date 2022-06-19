7b0d12f8b75409e805609aaef8fcf2ecec0e9c27
SimonGiampy <simongiampydev@gmail.com>
2022-06-19 23:34:31 +0200

    updated python script, definitive map added

--- scripts/trajectory_map.py
+++ scripts/trajectory_map.py
@@ -1,19 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 import numpy as np
 import cv2
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import Point
+from project2.srv import TrajectorySrv, TrajectorySrvResponse
 import os
 
 class Trajectory:
 	def __init__(self):
-		self.cwd = os.getcwd()  #script path 
+		self.cwd = os.getcwd()  #script path
 		rospy.Subscriber("/map", OccupancyGrid, self.callback_map)
 		rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.callback_trajectory)
+		rospy.Service('trajectory', TrajectorySrv, self.write_map)
 		self.map_ready = False
-	
+
+	# saves map data into a matrix
 	def callback_map(self, data):
 		self.width = data.info.width
 		self.height = data.info.height
@@ -21,16 +23,21 @@ class Trajectory:
 		self.image = np.zeros(size)
 
 		i = 0
-		for pixel in data.data:  #copy image
-			self.image[i% self.width][int(i/ self.width)] = pixel
+		for pixel in data.data:  #copy image into a matrix
+			self.image[i % self.width][int(i / self.width)] = pixel
 			i+=1
 
-		self.image[self.image == -1] = 50   #grey
+		self.image[self.image == -1] = 50   #gray
 		self.image[self.image == 0] = 255   #white
 		self.image[self.image == 100] = 0   #black
 
+		#parameters for calculating exact pixel positions
+		self.resolution = data.info.resolution
+		self.scaling = abs(data.info.origin.position.x) - self.width * self.resolution / 2.0;
+
 		self.map_ready = True
 
+	# create trajectory of points
 	def callback_trajectory(self, data):
 		if(self.map_ready):
 			x = data.pose.pose.position.x
@@ -39,11 +46,11 @@ class Trajectory:
 
 			#print("seq: " + str(data.header.seq) + "; x = " + str(x) + " -> " + str(int(x / 0.05 + self.width/2)) )
 			#print("seq: " + str(data.header.seq) + "; y = " + str(y) + " -> " + str(int(y / 0.05 + self.height/2)) )
-			
-			#self.image[(x+27.2)*0.05][(y+16)*0.05] = 50 
-			x_img = int((x + 5.6) / 0.05 + self.width/2)
-			y_img = int(y / 0.05 + self.height/2)
 
+			x_img = int((x + self.scaling) / self.resolution + self.width/2)
+			y_img = int(y / self.resolution + self.height/2)
+
+			# create a cross that points to the amcl pose
 			self.image[x_img][y_img] = 150
 			self.image[x_img+1][y_img] = 150
 			self.image[x_img-1][y_img] = 150
@@ -54,17 +61,14 @@ class Trajectory:
 			self.image[x_img][y_img+2] = 150
 			self.image[x_img][y_img-2] = 150
 
-
-
-
-
-			if(data.header.seq == 20):
-				filename = self.cwd + '/test_pixel3.png'
-				print ("saving image")
-				cv2.imwrite (filename,self.image)
+	def write_map(self, msg):
+		filename = os.path.abspath('..') + '/maps/map_trajectory.png'
+		print ("Saving image with map and trajectory")
+		cv2.imwrite(filename, self.image)
+		return TrajectorySrvResponse(True)
 
 
 if __name__ == '__main__':
-	rospy.init_node('depth_saver', anonymous=False)
+	rospy.init_node('trajectory', anonymous=False)
 	trajectory = Trajectory()
 	rospy.spin()
